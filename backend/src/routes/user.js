4a701a3004a781976029a9a4fa715fadf9e9ab27
hamza benarfa <hamzabenarfa4@gmail.com>
2024-07-08 22:38:36 +0100

    init

---
+++ backend/src/routes/user.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controller/user");
+
+router.get("/", getUsers);
+
+router.get("/:id", getUser);
+
+router.post("/", createUser);
+
+router.put("/:id", updateUser);
+
+router.delete("/:id", deleteUser);
+
+module.exports = router;
